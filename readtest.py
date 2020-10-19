2b13f0ba612df72604c9f97e4aa149b2ad84ceec
cristina <yosola@gmail.com>
2020-10-19 13:47:45 -0700

    Monday test with json

--- readtest.py
+++ readtest.py
@@ -7,30 +7,24 @@ import json
 
 
 #Path files
-PATH = '/Users/cristina/src/Alice_proni/Excel_files'
-
-#Return a list with all the csv files as objects
-def get_files(PATH):
-    files = []
-    for file in os.listdir(PATH):
-        files.append(file)
-    return(files)
-
-csvFileList = get_files(PATH)
-csvFilePath = os.path.join(PATH, csvFileList[0])
+PATH = '/Users/cristina/src/Alice_proni/Excel_files/'
+file = 'EPgrade1WBFill.csv'
+csv_file_path = os.path.join(PATH, file)
 
 
-#read the csv file and add to dict dat
+#read the csv file and add to dict data using "Instruccion" as key
 data = {}
-jsonFilePath = 'Test_EPgrade1WBFill.json'
+with open(csv_file_path) as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for csv_row in csv_reader:
+        key = csv_row["Instruccion"]
+        data[key] = csv_row
 
+#Adding the data to the root mode "fillblanks"
+root = {}
+root["fillblanks"] = data
 
-with open(csvFilePath) as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for rows in csvReader:
-        id = rows['Module']
-        data[id] = rows
-
-#Write the Json file
-with open(jsonFilePath, 'w') as JsonFile:
-    JsonFile.write(json.dumps(data, indent=4))
+#Write the data in a json files
+json_file_path = "EPgrade1WBFill.json"
+with open(json_file_path, "w") as json_file:
+    json_file.write(json.dumps(root,indent=4))
