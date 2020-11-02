84e1f1501e4ab149bb970c634b77b9c2bb6e238a
cristina <yosola@gmail.com>
2020-11-02 13:05:14 -0800

    DropDown edge cases

--- readdropquestions.py
+++ readdropquestions.py
@@ -4,31 +4,38 @@ import json
 import pandas as pd
 
 PATH = '/Users/cristina/src/Alice_proni/Excel_files/'
-file = 'EPgrade1WBDropdown.csv'
-input_csv_file_path = os.path.join(PATH, file)
+file = 'DropdownTestCases.csv'
+input = os.path.join(PATH, file)
 activities = []
 
 
-#Dropdown
-with open(input_csv_file_path) as csv_file:
-    csv_reader = csv.DictReader(csv_file)
 
+
+#Dropdown
+with open(input) as csv_file:
+    csv_reader = csv.DictReader(csv_file)
 
     for csv_row in csv_reader:
         #print(csv_row)
         if csv_row["Instruccion"]:
             #print(csv_row)
             activities.append({"title": csv_row["Instruccion"],
-                               "activity": []
+                               "questions": []
             })
 
         else:
-            question = {"question": csv_row["Pregunta"],
-                        "options": [csv_row["Opcion 1"], csv_row["Opcion 2"]],
-                        "correct": csv_row["Opcion Correcta"],
-                        "type": "text"}
-            activities[-1]["activity"].append(question)
+            if csv_row["Tipo"] == "text":
+                question = {"question": csv_row["Pregunta"],
+                            "options": [csv_row["Opcion 1"], csv_row["Opcion 2"], csv_row["Opcion 3"], csv_row["Opcion 4"], csv_row["Opcion 5"]],
+                            "correct": csv_row["Opcion Correcta"],
+                            "type": csv_row["Tipo"]}
+            else:
+                question = {"question": csv_row["Pregunta"],
+                            "options": [csv_row["Opcion 1"], csv_row["Opcion 2"], csv_row["Opcion 3"], csv_row["Opcion 4"], csv_row["Opcion 5"]],
+                            "correct": int(csv_row["Opcion Correcta"]),
+                            "type": csv_row["Tipo"]}
 
+            activities[-1]["questions"].append(question)
 
 #Write the data in a json files
 root = {"kind": "dropdown",
@@ -36,6 +43,6 @@ root = {"kind": "dropdown",
         "questions": activities
 }
 
-json_file_path = "EPgrade1WBDropdow.json"
+json_file_path = "DropdownTestCases.json"
 with open(json_file_path, "w") as json_file:
-    json_file.write(json.dumps(root,indent=2))
+    json_file.write(json.dumps(root,indent=4))
